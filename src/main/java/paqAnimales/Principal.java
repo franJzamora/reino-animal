da1c2d98bdcbf70028be3190f1714fb0c8df4669
franJzamora <zamorafranjimenez@gmail.com>
2025-03-09 11:55:37 +0100

    Subiendo proyecto de nuevo

--- src/main/java/paqAnimales/Principal.java
+++ src/main/java/paqAnimales/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
     public static void main(String[] args) {
 
 /*
-        Prueba
+        Prueba 2
 Esquema de clases de reino-animal        
                   Animal
                     |
